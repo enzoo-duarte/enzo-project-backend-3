e36ad02f0586c88d280e067a9ab8bcc1b9f770eb
Enzo Duarte <enzoduarte2019@gmail.com>
2025-09-16 03:40:20 -0300

    Configuracion de swagger, evolucion de logica de inicio del server ahora desde server.js para restringir la config del server a app.js. ajuste del script de inicio acorde a este cambio

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
+import __dirname from './utils/index.js';
+
 
 dotenv.config(); 
 
@@ -16,38 +18,50 @@ import mocksRouter from './routes/mocks.router.js';
 
 const app = express();
 
-const { PORT, JWT_SECRET, MONGO_URI } = process.env;
-console.log(PORT, JWT_SECRET);
-
-//MongoDB conexión
-mongoose.connect(MONGO_URI)
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
-
-// Middlewares
-app.use(express.json());
-app.use(cookieParser());
 
 // Swagger
 const swaggerOptions = {
     definition: {
         openapi: '3.0.1',
         info: {
-            title: 'API del poder y del saber',
-            description: 'Documentación de la API pensada para la clase de Swagger'
-        }
+            title: 'API AdoptMe',
+            version: '1.0.0',
+            description: 'Documentación de la API AdoptMe',
+        },
+        servers: [
+            {
+                url: "http://localhost:8080",
+                description: "Servidor de desarrollo",
+            },
+        ],
     },
     apis: [`${__dirname}/docs/**/*.yaml`]
 };
 
-const specs = swaggerJsDoc(swaggerOptions);
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
 // Rutas
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter);
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Bienvenido a la API AdoptMe</h1>
+        <p>Documentación disponible en <a href="/api-docs">/api-docs</a></p>
+    `);
+});
 
 export default app;
